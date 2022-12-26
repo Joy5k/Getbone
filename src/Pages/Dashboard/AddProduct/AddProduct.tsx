060a136707821d8e6a070f1c22897da5977dd9df
Joy <mmehedihasanjoyv@gmail.com>
2022-12-26 07:43:29 +0600

    added desktop fucntion and also get

--- src/Pages/Dashboard/AddProduct/AddProduct.tsx
+++ src/Pages/Dashboard/AddProduct/AddProduct.tsx
@@ -24,15 +24,15 @@ const AddProduct = () => {
   
   const handleAddProduct = (event: React.MouseEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const current = new Date();
-    const time = current.toLocaleDateString("en-BD");
-    setAddproduct({ ...addproduct,['publishDate']:time })
-    setAddproduct({ ...addproduct,['email']:user?.email })
+    // const current = new Date();
+    // const time = current.toLocaleDateString("en-BD");
+    // setAddproduct({...addproduct,['publishDate']:time })
+    setAddproduct({...addproduct,['email']:user?.email })
     console.log(addproduct)
     fetch('http://localhost:5000/addProduct', {
-      method: 'PUT',
+      method:'PUT',
       headers: {
-      'content-type': 'application/json'
+      'content-type':'application/json'
       },
       body:JSON.stringify(addproduct)
     
@@ -54,10 +54,19 @@ const AddProduct = () => {
     <div className="lg:w-10/12 md:w-10/12 sm:w-full mx-auto bg-white p-4">
  
 <form onSubmit={handleAddProduct}>
-    <div className="grid gap-6 mb-6 md:grid-cols-2 ">
+        <div className="grid gap-6 mb-6 md:grid-cols-2 ">
+        <div>
+          <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Product Category</label>
+          <select  onChange={handleOption} name='category' id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Doe" required >
+  <option  >Select</option>
+  <option   value="Desktop">Desktop</option>
+            <option value="Laptop">Laptop</option>
+            <option value="Phone">Phone</option>
+            </select>
+            </div>
         <div>
             <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product's Title</label>
-            <input onChange={handleChange} type="text" id="first_name" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Laptop name" required />
+            <input onChange={handleChange} type="text" id="first_name" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product model/name" required />
         </div>
         <div>
             <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Img field</label>
@@ -85,18 +94,7 @@ const AddProduct = () => {
             <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
             <input onChange={handleChange} type="text" id="visitors" name="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="The product is ..." required />
           </div>
-          <div>
-
-   
-          <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Product Category</label>
-          <select  onChange={handleOption} name='category' id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Doe" required >
-  <option  selected value="Desktop">Desktop</option>
-            <option value="Laptop">Laptop</option>
-            <option value="Phone">Phone</option>
-         
-  
-            </select>
-            </div>
+        
     </div>
     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Product</button>
 </form>
