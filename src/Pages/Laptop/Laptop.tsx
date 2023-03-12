f6cb896404951aecc53299ad5911ae2829b78470
Joy <mmehedihasanjoyv@gmail.com>
2023-03-12 16:34:06 +0600

    fixed the responsive problem for mobile device

--- src/Pages/Laptop/Laptop.tsx
+++ src/Pages/Laptop/Laptop.tsx
@@ -54,7 +54,7 @@ const Laptop=
   return (
       <div className='lg:w-10/12 md:w-10/12 sm:w-full mx-auto '>
           <h3 className='text-4xl font-bold text-blue-600 my-4'>Laptop</h3>
-          <div className='grid grid-cols-3 gap-4'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {Laptop}
           </div>
       </div>
