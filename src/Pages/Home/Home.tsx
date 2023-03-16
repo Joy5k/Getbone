c463098a2cda10bb69822b4dafb6aa58c023168c
Joy <mmehedihasanjoyv@gmail.com>
2023-03-16 23:54:46 +0600

    remove the search bar

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -70,25 +70,6 @@ const Home = () => {
 </div>)
   return (
     <div>
-      {/* search Bar */}
-
-          
-      
-<form onSubmit={()=>handleSearch} className="flex items-center w-full  md:w-10/12 lg:w-10/12  mx-auto">   
-    <label htmlFor="voice-search" className="sr-only">Search</label>
-    <div className="relative w-full">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-        </div>
-                  <input onChange={handleChange} type="text" id="voice-search" name='searchText'              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
-        <button  type="button" className="absolute inset-y-0 right-0 flex items-center pr-3">
-            <svg aria-hidden="true" className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd"></path></svg>
-        </button>
-    </div>
-    <button type="submit" className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-     Search
-    </button>
-          </form>
       
 <div id="default-carousel" className="relative sm:w-full md:w-10/12 lg:w-10/12 mx-auto bg-slate-400" data-carousel="slide">
     {/* <!-- Carousel wrapper --> */}
@@ -151,7 +132,7 @@ const Home = () => {
           
     <div  className='w-full h-96 relative md:w-10/12 lg:w-10/12 mx-auto '>
       <section className="dark:bg-gray-800 dark:text-gray-100 absolute  mb-12">
-<img className='w-full absolute h-full rounded-lg' src="https://www.uob.com.sg/assets/web-resources/personal/images/save/everyday-accounts/one-account/one-account-banner-lemon-1440x700.jpg" alt="Phone's Banner Image" />
+<img className='w-full absolute h-full rounded-lg' src="https://www.uob.com.sg/assets/web-resources/personal/images/save/everyday-accounts/one-account/one-account-banner-lemon-1440x700.jpg" alt=''/>
 </section>
         
           </div>
